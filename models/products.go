4eb8c03b6273cbe6b59884773ad2851b71c28bbb
João Paulo Fontana Macedo <jpfontana12@icloud.com>
2024-05-24 17:43:26 -0300

    feat: add user edition functionality

--- models/products.go
+++ models/products.go
@@ -17,7 +17,7 @@ type Product struct {
 func GetAllProduct() []Product {
 	db := db.InitDB()
 
-	rows, err := db.Query("SELECT * FROM produtos")
+	rows, err := db.Query("SELECT * FROM produtos ORDER BY id ASC")
 
 	if err != nil {
 		panic(fmt.Sprintf("Error querying the database: %v", err))
@@ -69,6 +69,42 @@ func DeleteProduct(id string) {
 	}
 
 	_, err = deleteData.Exec(id)
+
+	if err != nil {
+		panic(fmt.Sprintf("Error executing the statement: %v", err))
+	}
+
+	defer db.Close()
+}
+
+func GetProduct(id string) Product {
+	db := db.InitDB()
+
+	row := db.QueryRow("SELECT * FROM produtos WHERE id = $1", id)
+
+	p := Product{}
+
+	err := row.Scan(&p.ID, &p.Name, &p.Description, &p.Price, &p.Qtd)
+
+	if err != nil {
+		panic(fmt.Sprintf("Error scanning the database: %v", err))
+	}
+
+	defer db.Close()
+
+	return p
+}
+
+func UpdateProduct(id int, name string, description string, price float64, qtd int) {
+	db := db.InitDB()
+
+	updateData, err := db.Prepare("UPDATE produtos SET name = $1, description = $2, price = $3, qtd = $4 WHERE id = $5")
+
+	if err != nil {
+		panic(fmt.Sprintf("Error preparing the statement: %v", err))
+	}
+
+	_, err = updateData.Exec(name, description, price, qtd, id)
 
 	if err != nil {
 		panic(fmt.Sprintf("Error executing the statement: %v", err))
